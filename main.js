d92498ea543c3218b634019d6620da1bc1828860
EkaterinaKunskaya <nuar_23@mail.ru>
2022-07-12 19:38:56 +0300

    fix: fixed mistakes

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
+/*BURGER MENU*/
 const burgerMenuBtn = document.getElementsByClassName('burger-menu')[0];
-const headerNav = document.getElementsByClassName('header-nav')[0];
 const modalHeaderNav = document.getElementsByClassName('modal-header-nav-pop-up')[0];
+const headerNavBurger = document.getElementsByClassName('header-nav-burger')[0];
+const burgerClose = document.getElementsByClassName('burger-cross-close')[0];
+const navListBurger = document.getElementsByClassName('nav-list-burger')[0];
+
+const openBurger = () => {
+  headerNavBurger.classList.add('burger-active');
+  modalHeaderNav.style.display = "block";
+}
+
+const closeBurger = (event) => {
+  console.log(navListBurger)
+  if (event.target == modalHeaderNav || event.target == burgerClose || event.target.tagName === 'A') {
+    modalHeaderNav.style.display = "none";
+    headerNavBurger.classList.remove('burger-active');
+  }
+}
+
+burgerMenuBtn.addEventListener('click', openBurger);
+window.addEventListener('click', closeBurger);
+burgerClose.addEventListener('click', closeBurger);
+navListBurger.addEventListener('click', closeBurger);
 
 
-
+/*SLIDER*/
 const sliderList = document.getElementsByClassName('slider-list')[0];
 
 const sliderCircle1 = document.getElementsByClassName('slider-circle')[0];
@@ -16,38 +37,12 @@ const sliderImg3 = document.getElementsByClassName('slider-img')[2];
 
 let start = 0;
 
-/*Burger menu*/
-
-const headerNavBurger = document.getElementsByClassName('header-nav-burger')[0];
-const burgerClose = document.getElementsByClassName('burger-cross-close')[0];
-
-
-
-
-
-const openBurger = () => {
-  headerNavBurger.classList.add('burger-active');
-  modalHeaderNav.style.display = "block";
-}
-
-const closeBurger = (event) => {
-  if (event.target == modalHeaderNav || event.target == burgerClose) {
-    modalHeaderNav.style.display = "none";
-    headerNavBurger.classList.remove('burger-active');
-  }
-}
-
-burgerMenuBtn.addEventListener('click', openBurger);
-window.addEventListener('click', closeBurger);
-burgerClose.addEventListener('click', closeBurger);
-
 const scrollImg1 = () => {
         start = 800;
         sliderList.style.marginLeft = start + 'px';
         sliderCircle1.classList.add('circle-active');
         sliderCircle2.classList.remove('circle-active');
         sliderCircle3.classList.remove('circle-active');
-
 };
 
 const scrollImg2 = () => {
@@ -74,8 +69,8 @@ sliderImg1.addEventListener('click', scrollImg1);
 sliderImg2.addEventListener('click', scrollImg2);
 sliderImg3.addEventListener('click', scrollImg3);
 
-////////////////////////////////////////////////////
 
+/*MODAL*/
 // Получить модальный
 const modal = document.getElementsByClassName('modal-login-pop-up')[0];
 
@@ -89,7 +84,6 @@ const span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 
 
 // Когда пользователь щелкает в любом месте за пределами модального, закройте его
